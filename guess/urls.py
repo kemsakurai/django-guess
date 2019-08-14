59b0931b8a98849e1aa7fcbbaa5bd4f79fb65ffc
kemsakuai <sakurai.kem@gmail.com>
2019-08-15 00:18:59 +0900

    Add cached apis

--- guess/urls.py
+++ guess/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import GuessResultList
+from .views import GuessResultList, CachedGuessResultList
 
 urlpatterns = [
     url('^guessresult/(?P<pagepath>.+)/(?P<effectivetype>.+)/$', GuessResultList.as_view(), name='guess_result'),
     url('^guessresult/(?P<pagepath>.+)/$', GuessResultList.as_view(), name='guess_result_pagepath_only'),
+    url('^cachedguessresult/(?P<pagepath>.+)/(?P<effectivetype>.+)/$', CachedGuessResultList.as_view(), name='cached_guess_result'),
+    url('^cachedguessresult/(?P<pagepath>.+)/$', CachedGuessResultList.as_view(), name='cached_guess_result_pagepath_only'),
 ]
--- guess/views.py
+++ guess/views.py
@@ -3,6 +3,10 @@ from rest_framework import generics
 from .models import GuessResult
 from .serializer import GuessResultSerializer
 from .settings import GUESS_SETTINGS
+from rest_framework.response import Response
+from rest_framework import status
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 
 class GuessResultList(generics.ListAPIView):
@@ -14,6 +18,9 @@ class GuessResultList(generics.ListAPIView):
         the user as determined by the username portion of the URL.
         """
         page_path = self.kwargs.get('pagepath')
+        if not page_path:
+            return Response("page_path is required.", status=status.HTTP_400_BAD_REQUEST)
+
         effective_type = self.kwargs.get('effectivetype')
         print(effective_type)
         if not effective_type:
@@ -21,3 +28,10 @@ class GuessResultList(generics.ListAPIView):
         percent = GUESS_SETTINGS['PREFETCH_CONFIG'].get(effective_type);
 
         return GuessResult.objects.filter(previous_page_path=page_path, page_view_percent__gte=percent)
+
+
+class CachedGuessResultList(GuessResultList):
+    @method_decorator(cache_page(60 * 60 * 4))
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        return response
