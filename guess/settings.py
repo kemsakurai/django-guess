1e329608cba1f2786f4da12f0078206d296c49d4
kemsakuai <sakurai.kem@gmail.com>
2019-08-16 15:17:07 +0900

    change percent

--- guess/settings.py
+++ guess/settings.py
@@ -3,5 +3,5 @@ from django.conf import settings
 GUESS_SETTINGS = getattr(settings, "GUESS_SETTINGS", {})
 GUESS_SETTINGS.setdefault("VIEW_ID", "")
 GUESS_SETTINGS.setdefault("CREDENTIALS", getattr(settings, "BASE_DIR") + "/credentials.json")
-GUESS_SETTINGS.setdefault("PREFETCH_CONFIG", {'4g': 0.15, '3g': 0.3, '2g': 0.45, 'slow-2g': 0.6})
+GUESS_SETTINGS.setdefault("PREFETCH_CONFIG", {'4g': 15, '3g': 30, '2g': 45, 'slow-2g': 60})
 GUESS_SETTINGS.setdefault("COMMAND_CONFIG", {'RATIO_TO_EVALUATE': 0.95, 'LOWER_LIMIT_TRANSITION_PROBABILITY': 0.05})
